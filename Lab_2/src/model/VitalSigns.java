b20cd28a97acec459b4c427b89008365e86684a3
Omkar Salian <salian.o@northeastern.edu>
2024-09-22 12:41:29 -0400

    Lab_2 commit

---
+++ Lab_2/src/model/VitalSigns.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author omkarsalian
+ */
+public class VitalSigns {
+    String date;
+    double temperature;
+    double bloodPressure;
+    int pulse;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(double bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public int getPulse() {
+        return pulse;
+    }
+
+    public void setPulse(int pulse) {
+        this.pulse = pulse;
+    }
+    
+    @Override
+    public  String toString(){
+        return getDate();
+    }
+    
+}
