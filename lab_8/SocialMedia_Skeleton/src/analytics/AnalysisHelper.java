645e947570fe007ae43e7650e980100ba2fabdfb
O7580 <salian.o@northeastern.edu>
2024-11-21 16:19:53 -0500

    Q2

--- lab_8/SocialMedia_Skeleton/src/analytics/AnalysisHelper.java
+++ lab_8/SocialMedia_Skeleton/src/analytics/AnalysisHelper.java
@@ -29,5 +29,22 @@ public class AnalysisHelper {
         System.out.println("Average number of likes per comments: " + likeNumber / commentNumber);
             
     }
+    public void getMaxLikeCommentPost() {
+        DataStore data = DataStore.getInstance();
+        Comment commentWithMaxLikes = null;
+        
+        for (Comment c : data.getComments().values()){
+            if (commentWithMaxLikes == null){
+                commentWithMaxLikes = c;
+            }
+            if (c.getLikes() > commentWithMaxLikes.getLikes()){
+                commentWithMaxLikes = c;
+            }
+        }
+        
+        int postId = commentWithMaxLikes.getPostId();
+        
+        System.out.println("Q2 - post with most likes per comment " + data.getPosts().get(postId).getPostId());
+    }
     
 }
