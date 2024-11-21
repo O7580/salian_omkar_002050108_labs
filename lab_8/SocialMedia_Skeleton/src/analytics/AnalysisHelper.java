76b302739b3f70d1252bfc9948194cf89e6594a8
O7580 <salian.o@northeastern.edu>
2024-11-21 17:09:13 -0500

    Q5

--- lab_8/SocialMedia_Skeleton/src/analytics/AnalysisHelper.java
+++ lab_8/SocialMedia_Skeleton/src/analytics/AnalysisHelper.java
@@ -78,14 +78,37 @@ public class AnalysisHelper {
             } else {
                 postNumbers.put(userId, 1);
             }
-            
+
         }
         ArrayList<User> users = new ArrayList(data.getUsers().values());
         Collections.sort(users, new UserMapComparator(postNumbers));
         System.out.println("Q4- The following users have the least posts: ");
-        
-        for (int i = 0; i < 5; i++){
+
+        for (int i = 0; i < 5; i++) {
             System.out.println(users.get(i) + ", - Post count: " + postNumbers.get(users.get(i).getId()));
+        }
+    }
+
+    public void getPassiveCommentUsers() {
+        DataStore data = DataStore.getInstance();
+        HashMap<Integer, Integer> commentNumbers = new HashMap<Integer, Integer>();
+
+        for (Comment c : data.getComments().values()) {
+
+            int userId = c.getUserId();
+            if (commentNumbers.containsKey(userId)) {
+                commentNumbers.put(userId, commentNumbers.get(userId) + 1);
+            } else {
+                commentNumbers.put(userId, 1);
+            }
+
+        }
+        ArrayList<User> users = new ArrayList(data.getUsers().values());
+        Collections.sort(users, new UserMapComparator(commentNumbers));
+        System.out.println("Q5 - The following users have the least comments: ");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(users.get(i) + ", - Comment count: " + commentNumbers.get(users.get(i).getId()));
         }
     }
 
